4699dbf57a1f4c1cec501c51564412d17d69b91a
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-11-04 11:12:14 -0400

    Update Valid_sudoku.java

--- Valid_sudoku.java
+++ Valid_sudoku.java
@@ -44,23 +44,6 @@ class Solution {
 
 //Suggested solution
 
-//Solved by StefanPochmann -> https://leetcode.com/problems/valid-sudoku/discuss/15472/Short%2BSimple-Java-using-Strings
-
-
-public boolean isValidSudoku(char[][] board) {
-    Set seen = new HashSet();
-    for (int i=0; i<9; ++i) {
-        for (int j=0; j<9; ++j) {
-            if (board[i][j] != '.') {
-                String b = "(" + board[i][j] + ")";
-                if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
-                    return false;
-            }
-        }
-    }
-    return true;
-}
-
 
 // Solved by Lorraine921 -> https://leetcode.com/problems/valid-sudoku/discuss/15450/Shared-my-concise-Java-code
 
