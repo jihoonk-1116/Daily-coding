185ecc95ac6f6bd17ef3a50e4d0f9e0850e9ffa0
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-11-11 11:01:17 -0500

    Update BST with main

--- BST.java
+++ BST.java
@@ -1,44 +1,66 @@
-public class BST{
-
-    private TreeNode root;
-    
-    private class TreeNode{
-      
-      private int Data;
-      private TreeNode left;
-      private TreeNode Right;
-      
-      
-      public TreeNode(int data){
-        this.data = data;
-      }
-    }
-    public TreeNode insert(TreeNode node, int data){
-        if(node == null) { //Base case : assign node
-          node = new TreeNode(data);
-          return node;
-        }
-        if(data < node.data){ //if inserted integer data is less than the current value, 
-          node.left = insert(node.left, data); //then the data should be placed at the left side of the current node
-        }                                      //so, invoke insert method to move to the left
-        else{                 //if inserted integer data is less than the current value, 
-          node.right = insert(node.right, data);  //then the data should be placed at the right side of the current node
-        }                                         //so, invoke insert method to move to the right
-        return node;
-      }
-    public TreeNode searchByKey(TreeNode tn, int key){
+public class Tree {
+	public TreeNode root;
+	
+	private class TreeNode{
+		
+		private int data;
+		private TreeNode left;
+		private TreeNode right;
+		
+		public TreeNode(int value) {
+			this.data = value;
+		}
+		
+	}
+	public void insert(int value) {
+		root = insertNode(root, value);
+	}
+	
+	public TreeNode insertNode(TreeNode root, int value){
+		if(root == null) { 							//Base case : assign node
+			root = new TreeNode(value);
+			return root;
+		}
+		if(root.data > value) { 					//if inserted integer data is less than the current value, 
+			root.left=insertNode(root.left, value); //then the data should be placed at the left side of the current node
+		}											//so, invoke insert method to move to the left
+		else 										//if inserted integer data is less than the current value, 
+			root.right = insertNode(root.right, value);  //then the data should be placed at the right side of the current node
+		 												//so, invoke insert method to move to the right
+		return root;
+		
+	}
+	
+	public void searchByKey(int key) {
+		TreeNode temp = searchByKey(this.root, key);
+		if(temp == null) System.out.println("Not found...");
+		else System.out.println("Found! : " + temp.data);
+		
+	}
+	private TreeNode searchByKey(TreeNode tn, int key){
         if(tn == null || tn.data == key) return tn; //base case
+        
         if(key < tn.data) return searchByKey(tn.left, key); //Search left in order to finding the value that is less than root
-        else return searchByKey(tn.right. key); //Search right in order to finding the value that is bigger than root
+        else return searchByKey(tn.right, key); //Search right in order to finding the value that is bigger than root
                                      
     }
-    
-    public void inorder(TreeNode node){ //inorder traveral (left - parent - right)
-        if(node == null){
-          return;
-        }
-        inorder(node.left);
-        System.out.println(node.data);
-        inorder(node.right);
-  }
+	public void inorder(TreeNode root) {
+		if(root == null) return;
+		
+		inorder(root.left);
+		System.out.println(root.data);
+		inorder(root.right);
+		
+	}
+	public static void main(String[] args) {
+		Tree bst = new Tree();
+		bst.insert(4);
+		bst.insert(2);
+		bst.insert(3);
+		bst.insert(1);
+		bst.insert(5);
+		bst.insert(6);
+		bst.inorder(bst.root);
+		bst.searchByKey(0);
+	}
 }
