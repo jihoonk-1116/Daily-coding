3cf593009d0caf014d210ac2b837c5eac401212d
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-11-15 14:05:27 -0500

    Update ReverseString3.java

--- ReverseString3.java
+++ ReverseString3.java
@@ -1,3 +1,16 @@
+/*
+https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+*/
+
+
 class Solution {
     public String reverseWords(String s) {
         //use two pointers
