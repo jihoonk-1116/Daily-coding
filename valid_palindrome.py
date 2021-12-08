f670fa3cd2a017d87c8d1defd68b0a1a2830dbf6
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-12-07 23:54:09 -0500

    Update valid_palindrome.py

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -13,4 +13,17 @@ class Solution:
         
         return True
       
- #2
+ #2 - use Deque
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        result : Deque = collections.deque()
+            
+        for c in s:
+            if c.isalnum():
+                result.append(c.lower())
+                
+        while len(result)>1:
+            if result.popleft() != result.pop():
+                return False
+        
+        return True
