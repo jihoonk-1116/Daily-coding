b32d7f29a707083ffd70922897ccfc1c42eb2a6c
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-12-14 22:08:54 -0500

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
         num_dict = dict()   #map
         for i, num in enumerate(nums):
             num_dict[num] = i   #Numbers become key, and indeces become value
@@ -8,3 +9,17 @@ class Solution:
             complement = target - num 
             if complement in num_dict and i != num_dict[complement]: #find complement as a key in the map, and not the same index
                 return [i, num_dict[complement]]
+
+ 
+#One for loop but same time complexity
+
+class Solution:
+    def twoSum(self, nums:List[int], target: int) -> List[int]:
+        
+        num_dict = dict()
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [i, num_dict[complement]]
+            num_dict[nums] = i
