f6c3d9f03bdc6fd7d950b2c4ac4face7562a6817
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-12-14 22:01:32 -0500

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,10 +1,10 @@
 class Solution:
-   def twoSum(self, nums: List[int], target: int) -> List[int]:
-       seen = {}
-       for i, value in enumerate(nums): 
-           remaining = target - nums[i] 
-           
-           if remaining in seen: 
-               return [i, seen[remaining]]  
-           else:
-               seen[value] = i  
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = dict()   #map
+        for i, num in enumerate(nums):
+            num_dict[num] = i   #Numbers become key, and indeces become value
+        
+        for i,num in enumerate(nums):
+            complement = target - num 
+            if complement in num_dict and i != num_dict[complement]: #find complement as a key in the map, and not the same index
+                return [i, num_dict[complement]]
