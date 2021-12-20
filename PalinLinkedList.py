7febfce5f3af1011c37835645dce91e420d36fd3
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-12-19 22:34:56 -0500

    Update PalinLinkedList.py

--- PalinLinkedList.py
+++ PalinLinkedList.py
@@ -14,7 +14,29 @@ class Solution:
             node = node.next
         
         while len(q)>1:
-            if q.pop(0) != q.pop():
+            if q.pop(0) != q.pop():   #q.pop() takes O(n) because list is singly link list
                 return False
         
         return True
+
+    
+#Approach 2 - using deque that is doubly linked list to access to both end side of the list with O(1)
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        dq = deque()
+        
+        if not head:
+            return True
+        
+        node = head
+        while node is not None:
+            dq.append(node.val)
+            node = node.next
+            
+        while len(dq) > 1:
+            if dq.pop() != dq.popleft():   
+                return False
+            
+        return True
+    
+# solution#2 takes 844 ms whereas solution#1 takes 2302ms.  
