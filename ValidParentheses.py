2f266ef1f1a803f63bd8300e76810d7b6b4bfb27
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-12-29 22:54:26 -0500

    Create ValidParentheses.py

---
+++ ValidParentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        table = {
+            ')':'(',
+            '}':'{',
+            ']':'[',
+        }
+        
+        for c in s:
+            if c not in table:
+                stack.append(c)
+            elif not stack or table[c] != stack.pop():
+                return False
+        
+        return len(stack)==0
+      
+  
