9691051a8dba9355590738cf9909709b2253fb06
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-01-05 23:18:10 -0500

    Update ValidParentheses.py

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,6 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
+        stack = list()
+        #mapping table
         table = {
             ')':'(',
             '}':'{',
@@ -8,11 +9,10 @@ class Solution:
         }
         
         for c in s:
-            if c not in table:
-                stack.append(c)
-            elif not stack or table[c] != stack.pop():
-                return False
+            if c not in table: #find c as a key 
+                stack.append(c) #c is not key then append to the stack
+            elif not stack or table[c] != stack.pop(): #if the stack is empty 
+                                                       #or the value mapping to c is not equal to the last inserted element of the stack
+                return False                           #return false
         
-        return len(stack)==0
-      
-  
+        return len(stack)==0 #if the stack is empty after the loop -> true
