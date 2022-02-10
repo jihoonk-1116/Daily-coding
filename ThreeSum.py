d94c29bc9196a1e12a33f05c7022ca1ade56e6db
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-02-09 23:15:28 -0500

    add more comments to ThreeSum.py

--- ThreeSum.py
+++ ThreeSum.py
@@ -1,31 +1,47 @@
-#Find three value's indics that thier summation becomes 0
+#Find three values that their summation becomes 0
 #Two pointer approach : O(n^2)
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        results = []
-        nums.sort() # sort by ascending order
+        #Three sum = 0
+        #return triplets in list
+        #1. Sort nums
+        #2. Iterate nums with i 
+        #3. Nested loop with left that starts i+1 and right that begins len(nums)-1
         
-        for i in range(len(nums) -2):
-            if i>0 and nums[i] == nums[i-1]: #skip when the current i is the same value the previous one. Because the case is already checked before i
-                continue      
-            left, right = i+1, len(nums)-1 #left is always the next to the i
+        result = []
+        nums.sort() #inplace sorting in ascending order
+        
+        for i in range(len(nums)-2):
+            #Skip when the next i is same as the previous one
+            if i > 0 and nums[i] == nums[i-1]:
+                continue 
+            left , right = i+1, len(nums)-1
             while left < right:
-                three_sum = nums[i] + nums[left] + nums[right]
-                if three_sum > 0 : 
-                    right -= 1    #Find smaller value as decrease right index
-                elif three_sum < 0 :
-                    left += 1     #Find greater value as increase left index
-                else :
-                    results.append([nums[i],nums[left], nums[right]])
+                sum = nums[i] + nums[left] + nums[right]
+                #if sum is less than 0, then it needs to be increased
+                #So, move left because nums is already sorted
+                if sum < 0:
+                    left += 1
                     
-                    while left < right and nums[left] == nums[left+1]: #skip when the next to left is same as the current left
-                        left +=1
-                    while left < right and nums[right] == nums[right-1]: #skip when the next to right is same as the current right
-                        right -=1
-                        
-                    #assign new value to the both
-                    left+=1 
-                    right-=1
+                #if sum is greater than 0, then it needs to be decreased
+                #So, move right because nums is already sorted
+                elif sum > 0:
+                    right -= 1
+                    
+                #if a triplet is found
+                else:
+                    #append it to result list
+                    result.append([nums[i],nums[left], nums[right]])
+                    
+                    #Skip the next left value when the current left's value is equal to the next one, and right pointer too.
+                    while left<right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    
+                    left += 1
+                    right -= 1
+            
+        return result
         
-        return results
+        
