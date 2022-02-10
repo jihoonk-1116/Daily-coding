5c35dc0fdfa40bd4521ab469659d5eb6f3f0a214
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-02-09 23:41:37 -0500

    Update ArrayParition1.py

--- ArrayParition1.py
+++ ArrayParition1.py
@@ -1,12 +1,28 @@
 #Find n pair's max sum of min(a,b)
-#Sorted array's even index becomes max of min
+#Sorted list's even index becomes max of min
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+       
+        sum = 0
+        pair = []
+        nums.sort()
+        
+        for num in nums:
+            #append two values to pair list
+            pair.append(num)
+            if len(pair) == 2:
+                sum += min(pair) #add minimum value of pair to sum
+                pair = []        #re-initialize pair list
+        
+        return sum
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         sum =0
         nums.sort()
         for i, n in enumerate(nums):
-            if i%2 ==0:
+            if i%2 ==0: #Add only even index's value to sum because nums is sorted
                 sum += n
         return sum
       
