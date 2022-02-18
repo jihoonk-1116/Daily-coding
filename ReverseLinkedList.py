194db2d494990864db39b39963f575395a02e0b7
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-02-17 23:35:12 -0500

    Update ReverseLinkedList.py

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -6,7 +6,15 @@
 #         self.val = val
 #         self.next = next
 
+
+'''
+1. save node.next in next
+2. assign node.next to prev
+3. assign node to next
+4. assign prev to node
+'''
 #Approach1 : Recursive call with next node that is being handled in the next call and current node that is being assigned next of next
+# Which data shold be passed for the next recursion. 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         def reverse(node : ListNode, prev : ListNode=None):
@@ -14,11 +22,11 @@ class Solution:
                 return prev
             
             next, node.next = node.next, prev
-            #The next variable is for storing the next element to be covered because node.next should be 
-            #assigned prev 
+            #The next variable is for storing the next element to be covered in the next invocation
+            #To reverse, node.next should be prev
             return reverse(next, node)
-            #reverse function is invoked with next argument as current node 
-            #and the input node as prev argument
+            #next becomes the node argument that points to the current position for the next recursion.
+            #node becomes the prev argument being assigned to in front of the node for the next recursion.
             
         return reverse(head)
       
