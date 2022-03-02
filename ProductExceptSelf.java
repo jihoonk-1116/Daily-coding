300c85d02e7e662a062953b39c5c9828a45ad9a9
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-03-02 16:05:50 -0500

    Create ProductExceptSelf.java

---
+++ ProductExceptSelf.java
@@ -0,0 +1,19 @@
+//Time : O(n), Space : O(n)
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int left = 1, right = 1;
+        int[] ans = new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++){
+            ans[i] = left;
+            left *= nums[i];
+        }
+        
+        for(int i=nums.length-1;i >= 0;i--){
+            ans[i] *= right;
+            right *= nums[i];
+        }
+        return ans;
+    }
+}
