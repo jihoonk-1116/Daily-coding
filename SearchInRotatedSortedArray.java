5260ca95830fb8939bd0e790f2f34ed60090f26b
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-03-08 19:07:37 -0500

    Update SearchInRotatedSortedArray.java

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -9,20 +9,22 @@ class Solution {
                 return m;
             }
             
-            if(nums[left] <= nums[m]){ //left sorted portion.
+            if(nums[left] <= nums[m]){ //Look up left sorted portion.
                 
                 //nums[left] is the smallest value in range of left - m
                 //if target is out of the bound from left - m 
-                //then move left pointer m+1
+                //then move left pointer to m+1 
+                //otherwise, right becomes m-1 to iterate this left sorted portion
                 if( target < nums[left] || target > nums[m]) left = m + 1;
                 else right = m - 1;
             }
             
-            else{  //right sorted portion.
+            else{  //Look up right sorted portion.
                 
                 //nums[m] is the smallest value in the range of m - right
                 //if target is out of the bound from m - right 
-                //then move right pointer m-1
+                //then move right pointer to m-1
+                //otherwise, left becomes m+1 to iterate this right sorted portion
                 if(target < nums[m] || target > nums[right]) right = m - 1;
                 else left = m + 1;
             }
