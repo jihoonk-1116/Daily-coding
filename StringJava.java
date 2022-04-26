2e5268ec4e657e188e5c9bef88d259dd12275eb6
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-04-25 21:35:35 -0400

    Update StringJava.java

--- StringJava.java
+++ StringJava.java
@@ -1,4 +1,6 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class p1 {
@@ -48,13 +50,14 @@ public class p1 {
             String ans = "";
             int pos = 0;
 
-            while ((pos = s.indexOf(' ')) != -1) {
+            while ((pos = s.indexOf(' ')) != -1) { // indexOf(' ') -> return an index that is the first space
+                // if -1 , then there is no more space!
 
                 String tmp = s.substring(0, pos);
                 if (ans.length() < tmp.length()) {
                     ans = tmp;
                 }
-                s = s.substring(pos + 1);
+                s = s.substring(pos + 1); // update s to find the next space
             }
             if (ans.length() < s.length()) {
                 ans = s;
@@ -122,7 +125,7 @@ public class p1 {
      */
     static public class Q4 {
 
-        private String[] strArr = { "hi", "jihoon", "great", "sam" };
+        private String[] strArr = {"hi", "jihoon", "great", "sam"};
 
         public Q4() {
         }
@@ -240,6 +243,7 @@ public class p1 {
         }
 
         public void q7_call() {
+
             System.out.println(q7());
             System.out.println("q7 =====" + q7_3());
         }
@@ -292,6 +296,72 @@ public class p1 {
             return false;
 
         }
+    }
+
+    public static class Q8 {
+
+        public Q8() {
+
+        }
+
+        // 자연수 추출
+        public void q8() {
+            String str = "g0en2T0s8eSoft";
+            String ans = "";
+
+            for (char c : str.toCharArray()) {
+                if (Character.isDigit(c))
+                    ans += c;
+            }
+            System.out.println(Integer.valueOf(ans));
+            System.out.println(Integer.valueOf(str.replaceAll("[^0-9]", "")));
+        }
+
+    }
+
+    public static class Q9 {
+        public Q9() {
+
+        }
+
+        // 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리
+        public void q9() {
+
+            String str = "teachermode";
+            char letter = 'e';
+            int[] ans = new int[str.length()];
+            int p = 1000;
+
+            for (int i = 0; i < str.length(); i++) {
+                p++;
+                if (str.charAt(i) == letter) {
+                    p = 0;
+                    ans[i] = 0;
+                } else {
+                    ans[i] = p;
+                }
+            }
+
+            p = ans[ans.length - 1];
+
+            for (int i = str.length() - 1; i >= 0; i--) {
+
+                p++;
+
+                if (str.charAt(i) != letter && ans[i] > p) {
+                    ans[i] = p;
+                    /// ans[i] = Math.min(ans[i], p);
+                } else {
+                    p = 0;
+                }
+            }
+
+            for (int n : ans) {
+                System.out.print(n + " ");
+            }
+        }
+
+        // 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는 문자 바로 오른쪽에 반복 횟수를 표기하는 방법
         public void q10() {
 
             String str = "KKHSSSSSSSE";
@@ -320,20 +390,20 @@ public class p1 {
             System.out.println(ans.toString().replaceAll("[^2-9^A-Z]", ""));
         }
 
-        public void q10_1(){
+        public void q10_1() {
             String str = "KKHSSSSSSSE";
 
             String ans = "";
-            int count=1;
+            int count = 1;
 
             str = str + " ";
-            for(int i =0;i<str.length()-1;i++){
-                if(str.charAt(i) == str.charAt(i+1)){
+
+            for (int i = 0; i < str.length() - 1; i++) {
+                if (str.charAt(i) == str.charAt(i + 1)) {
                     count++;
-                }
-                else{
+                } else {
                     ans += str.charAt(i);
-                    if(count == 1){
+                    if (count == 1) {
                         continue;
                     }
                     ans += String.valueOf(count);
@@ -342,11 +412,175 @@ public class p1 {
             }
             System.out.println(ans);
         }
-    }
 
-    public static void main(String[] args) {
+        /**
+         * n = 4 , s = #****###**#####**#####**##**
+         * 7 digits
+         * # -> 1 , * -> 0
+         */
+        public void q11() {
+            int n = 4;
+            String str = "#****###**#####**#####**##**";
+            String ans = "";
 
-        Q7 q7 = new Q7();
-        q7.q7_call();
+            for (int i = 0; i < n; i++) {
+                String tmp = str.substring(0, 7);
+                tmp = tmp.replace("#", "1")
+                        .replace("*", "0");
+                System.out.println(tmp);
+                //System.out.println((char) Integer.parseInt(tmp,2));
+                ans += (char) Integer.parseInt(tmp, 2);
+                str = str.substring(7);
+            }
+            System.out.println(ans);
+        }
+
+        /**
+         * 5
+         * 87 89 92 100 76
+         * out - 43215
+         */
+        public void q12(int[] arr) {
+
+            int[] ans = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+
+                int cnt = 1;
+
+                for (int j = 0; j < arr.length; j++) {
+
+                    if (arr[i] < arr[j]) cnt++;
+                }
+
+                ans[i] = cnt;
+
+            }
+            for (int i : ans) {
+                System.out.println("i = " + i);
+            }
+        }
+
+        /**
+         * 10 13 10 12 15
+         * 12 39 30 23 11
+         * 11 25 50 53 15
+         * 19 27 29 37 27
+         * 19 13 30 13 19
+         * <p>
+         * out - 155
+         */
+        public void q13(int n) {
+            int[][] input = {
+                    {10, 13, 10, 12, 15},
+                    {12, 39, 30, 23, 11},
+                    {11, 25, 50, 53, 15},
+                    {19, 27, 29, 37, 27},
+                    {19, 13, 30, 13, 19}
+            };
+            int sum1, sum2, ans = 0;
+            for (int i = 0; i < n; i++) {
+                sum1 = sum2 = 0;
+                for (int j = 0; j < n; j++) {
+                    sum1 += input[i][j];
+                    sum2 += input[j][i];
+                }
+                ans = Math.max(sum1, ans);
+                ans = Math.max(sum2, ans);
+            }
+            sum1 = sum2 = 0;
+            for (int i = 0; i < n; i++) {
+                sum1 += input[i][i];
+                sum2 += input[i][n - i - 1];
+            }
+
+            ans = Math.max(sum1, ans);
+            ans = Math.max(sum2, ans);
+
+            System.out.println("ans = " + ans);
+        }
+
+        /**
+         * 53723
+         * 37161
+         * 72534
+         * 43641
+         * 87352
+         * out - 10
+         */
+        public void q14() {
+            int[][] input = {
+                    {5, 3, 7, 2, 3},
+                    {3, 7, 1, 6, 1},
+                    {7, 2, 5, 3, 4},
+                    {4, 3, 6, 4, 1},
+                    {8, 7, 3, 5, 2}
+            };
+            //         (0,1)
+            //           ^
+            // (-1,0) <     > (1,0)
+            //           |
+            //         (0,-1)
+            int[] dx = {-1, 0, 1, 0};
+            int[] dy = {0, 1, 0, -1};
+            int ans = 0;
+
+            for (int i = 0; i < input.length; i++) {
+                for (int j = 0; j < input.length; j++) {
+
+                    boolean flag = true;
+                    for (int k = 0; k < 4; k++) {
+
+                        int cx = i + dx[k];
+                        int cy = j + dy[k];
+
+                        if (cx >= 0 && cx < input.length && cy >= 0 && cy < input.length) {
+                            if (input[cx][cy] >= input[i][j]) {
+                                flag = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (flag == true) {
+                        ans++;
+                    }
+                }
+            }
+            System.out.println("ans = " + ans);
+        }
+
+        public void q15() {
+            int[][] input = {
+                    {2, 3, 1, 7, 3},
+                    {4, 1, 9, 6, 8},
+                    {5, 5, 2, 4, 4},
+                    {6, 5, 2, 6, 7},
+                    {8, 4, 2, 2, 2}
+            };
+            int ans = 0;
+            for (int curStu = 0; curStu < input.length; curStu++) {
+                int count = 0;
+                for (int compStu = 0; compStu < input.length; compStu++) {
+                    for (int stuClass = 0; stuClass < input.length; stuClass++) {
+                        if (input[curStu][stuClass] == input[compStu][stuClass]) {
+                            count++;
+                            break;
+                        }
+                    }
+                }
+                if (ans < count) {
+                    ans = count;
+                }
+            }
+            System.out.println("ans = " + ans);
+
+        }
+
+        public static void main(String[] args) {
+
+            Q9 q9 = new Q9();
+            //q9.q12(new int[]{87,89,92,100,76});
+            q9.q15();
+        }
     }
 }
