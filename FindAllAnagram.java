93dabeb212b9871f805de62943e7218635e7f28e
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-06-11 13:14:31 -0400

    Update FindAllAnagram.java

--- FindAllAnagram.java
+++ FindAllAnagram.java
@@ -52,3 +52,47 @@ public class FindAllAnagram {
     
     }
 }
+//leetcode
+class Solution {
+    public List<Integer> findAnagrams(String word, String pattern) {
+        
+        ArrayList<Integer> indicesOfAna = new ArrayList<>();
+        
+        //use two hashmap
+        HashMap<Character, Integer> wordMap = new HashMap<>();
+        HashMap<Character, Integer> patternMap = new HashMap<>();
+        
+        //fill up a map with pattern 
+        for(char c : pattern.toCharArray()){
+            patternMap.put(c,patternMap.getOrDefault(c, 0) + 1);
+        }
+        //for loop
+        int l =0;
+        for(int r=0;r<word.length();r++){
+             //pre-fill up another map with word as len(word) - 1
+            if(r < pattern.length()-1){
+                wordMap.put(word.charAt(r), 
+                            wordMap.getOrDefault(word.charAt(r), 0) + 1);
+            }
+            else{
+                //add one more character to the map for word
+                wordMap.put(word.charAt(r), 
+                            wordMap.getOrDefault(word.charAt(r), 0) + 1);
+                
+                //compare two maps -> if same, then count++
+                //otherwise, do nothing and go to next
+                if(wordMap.equals(patternMap)) indicesOfAna.add(l);
+                
+                //before the next, take out one character from word map
+                wordMap.put(word.charAt(l), wordMap.get(word.charAt(l)) - 1);
+                if(wordMap.get(word.charAt(l)) == 0)
+                    wordMap.remove(word.charAt(l));
+                
+                l++;
+                
+            }
+        }
+        
+        return indicesOfAna;
+    }
+}
