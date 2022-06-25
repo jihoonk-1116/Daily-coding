130b4b9d58868ded386d4d7899efec68f612fa65
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-06-25 14:22:39 -0400

    Create ConstructBinaryTreefromPreorderInorderTraversal.java

---
+++ ConstructBinaryTreefromPreorderInorderTraversal.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+//two different array expressions to represent a tree
+//preoder[0] means a root of the tree
+//By on the root, we can identify where right & left sub trees' elements are
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+    }
+}
