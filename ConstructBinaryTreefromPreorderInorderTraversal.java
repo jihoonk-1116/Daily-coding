c290be876277b4a4a8079127c9364dde40cd00d7
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-06-26 19:47:54 -0400

    Update ConstructBinaryTreefromPreorderInorderTraversal.java

--- ConstructBinaryTreefromPreorderInorderTraversal.java
+++ ConstructBinaryTreefromPreorderInorderTraversal.java
@@ -21,5 +21,24 @@
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         
+        if(inorder.length==0 || preorder.length==0) return null;
+        if(preorder.length == 1) return new TreeNode(preorder[0]);
+        
+        TreeNode root = new TreeNode(preorder[0]);
+        
+        int mid = 0;
+        for(int i =0;i<inorder.length;i++){
+            if(inorder[i] == preorder[0]){
+                mid = i;
+                break;
+            }
+        }
+        root.left = buildTree(Arrays.copyOfRange(preorder, 1, mid+1), Arrays.copyOfRange(inorder,0,mid));
+    
+        root.right = buildTree(Arrays.copyOfRange(preorder, mid+1, preorder.length), Arrays.copyOfRange(inorder, mid+1, inorder.length));
+        
+        return root;
     }
+    
+    
 }
