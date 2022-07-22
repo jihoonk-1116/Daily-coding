71e4888962193eca047395fdf3b67c13790bba3e
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-07-22 17:33:29 -0400

    Update PermutationInString.java

--- PermutationInString.java
+++ PermutationInString.java
@@ -1,38 +1,69 @@
 class Solution {
+    
+//     public boolean checkInclusion(String s1, String s2) {
+        
+//         if (s1.length() > s2.length()) return false;
+        
+//         int[] charHash1 = new int[26];
+//         int[] charHash2 = new int[26];
+        
+//         for(int i=0;i<s1.length();i++){
+//             charHash1[s1.charAt(i) - 'a'] ++;
+//             charHash2[s2.charAt(i) - 'a'] ++;
+//         }
+     
+//         int lt = 0;
+//         for(int rt=s1.length();rt<s2.length();rt++){
+//             if(matches(charHash1, charHash2)) return true;
+//             else{ //move window 
+//                 charHash2[s2.charAt(rt) - 'a']++;
+//                 charHash2[s2.charAt(lt) - 'a']--;
+//                 lt++;
+//             }
+            
+//         }
+//         return matches(charHash1, charHash2);
+//     }
+//     private boolean matches(int[] map1, int[] map2){
+//         for(int i= 0;i<26;i++){
+//             if(map1[i] != map2[i]) return false;
+//         }
+//         return true;
+//     }
+         
+    
+    
     public boolean checkInclusion(String s1, String s2) {
-        //find permutation first
-        //use two hash
-        //check if they are equal -> true 
+        if(s1.length()> s2.length()) return false;
         
-        if (s1.length() > s2.length()) return false;
+        int[] charMap = new int[26];
+        for(int i=0;i<s1.length();i++){
+            charMap[s1.charAt(i)-'a']++;
+            charMap[s2.charAt(i)-'a']--;
+        }
         
-        int[] charHash1 = new int[26];
-        int[] charHash2 = new int[26];
+        int matches = 0;
+        for(int i=0;i<26;i++){
+            if(charMap[i] == 0) matches ++;
+        }
         
-        //iterate thru s2 
-        //use window to find permutation substring in s2 with size of s1
         int lt = 0;
-        for(int rt=0;rt<s2.length();rt++){
-            if(rt < s1.length()){ //fill up the hash maps by s1.len
-                charHash1[s1.charAt(rt) -'a']++;
-                charHash2[s2.charAt(rt) -'a']++;
-            }
-            else if(matches(charHash1, charHash2)) return true;
-            else{ //move window 
-                System.out.println(rt);
-                charHash2[s2.charAt(rt) - 'a']++;
-                charHash2[s2.charAt(lt) - 'a']--;
-                lt++;
-            }
+        for(int rt=s1.length();rt<s2.length();rt++){
+            
+            if(matches == 26) return true;
+            
+            int curLetterKey = s2.charAt(rt) - 'a';
+            charMap[curLetterKey]--;
+            if(charMap[curLetterKey] == 0) matches++;
+            else if(charMap[curLetterKey] == -1) matches --;
+            
+            int leftLetterKey = s2.charAt(lt++) - 'a';
+            charMap[leftLetterKey] ++;
+            if(charMap[leftLetterKey] == 0) matches ++;
+            else if(charMap[leftLetterKey] == 1) matches --; 
             
         }
-        return matches(charHash1, charHash2);
-    }
-    private boolean matches(int[] map1, int[] map2){
-        for(int i= 0;i<26;i++){
-            if(map1[i] != map2[i]) return false;
-        }
-        return true;
+        
+        return matches == 26;
     }
 }
-               
