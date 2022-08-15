fde4cbfbadb37ce61dcd3d09d97731002c2f67f0
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2022-08-15 17:16:42 -0400

    Update MinimumWindowSubstring.java

--- MinimumWindowSubstring.java
+++ MinimumWindowSubstring.java
@@ -1,54 +1,42 @@
 class Solution {
-
     public String minWindow(String s, String t) {
-        //find minimun window that includes t
-        //hash map comparing? 
         
-       
+        if(s.length() < t.length()) return "";
         
-        if (s.length() == 0 || t.length() == 0) {
-            return "";
+        int matches = 0;
+        int[] tHash = new int[64];
+        int[] sHash = new int[64];
+        
+        for(int i=0;i<t.length();i++){
+            tHash[t.charAt(i)-'A']++;
+            sHash[s.charAt(i)-'A']++;
+        }
+        for(int i=0;i<64;i++) {
+            if(tHash[i] > 0) matches++;
+            if(sHash[i] > 0 && tHash[i] > 0 && tHash[i] <= sHash[i]) matches--;
         }
         
-        HashMap<Character, Integer> mapS = new HashMap<>();
-        HashMap<Character, Integer> mapT = new HashMap<>();
+        if(matches == 0) return s.substring(0,t.length());
         
-        for(char c : t.toCharArray()){
-            mapT.put(c, mapT.getOrDefault(c, 0) + 1);
-        }
-        
-        int have = 0, need = mapT.size();
-        int minLen = Integer.MAX_VALUE;
-        int lt = 0, resLt = 0;
-        
-        for(int rt=0;rt<s.length();rt++){
+        int lt=0, ansLt = 0, ansRt = 0, len = s.length()+1;
+        for(int rt=t.length();rt<s.length();rt++){
             
-            char c = s.charAt(rt);
-            mapS.put(c, mapS.getOrDefault(c,0) + 1);
-            
-            if(mapT.containsKey(c) && mapT.get(c) == mapS.get(c)){
-                have++;
-            }
-            //aaabbbbccdd
-            while(have == need){ //all characters in t is found 
-                //update result
-                if((rt - lt + 1) < minLen){
-                    resLt = lt;
-                    minLen = rt - lt + 1;
+            char token = s.charAt(rt);
+            sHash[token -'A']++;
+            if(sHash[token-'A'] == tHash[token-'A']) matches--;
+        
+            while(matches == 0){
+                if(len > (rt-lt+1)){
+                    ansLt = lt;
+                    ansRt = rt;
+                    len = rt-lt+1;
                 }
-                //pop left
-                //update window map
-                mapS.put(s.charAt(lt), mapS.get(s.charAt(lt)) - 1);
-                if(mapT.containsKey(s.charAt(lt)) &&
-                   mapT.get(s.charAt(lt)) > mapS.get(s.charAt(lt))){
-                       have --;
-                }
-                lt++;
+                token = s.charAt(lt++);
+                sHash[token-'A']--;
+                if(tHash[token-'A'] > 0 && sHash[token-'A'] < tHash[token-'A']) matches++;
             }
-            
         }
-                   
-        if(minLen > s.length()) return "";
-        else return s.substring(resLt, resLt+minLen);
+        
+        return len == s.length()+1 ? "" : s.substring(ansLt, ansRt+1);
     }
 }
